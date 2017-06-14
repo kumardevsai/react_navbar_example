e4a89519e981074a17da712b27bda3ee3cf82e7e
sepler <guitarobot@gmail.com>
2017-06-14 19:11:47 -0400

    updated

--- src/pages/ExamplePages.js
+++ src/pages/ExamplePages.js
@@ -1,39 +1,63 @@
 import React, { Component } from 'react';
 
-function ret() {
-  return (
-    <div className="page">
-      <h1>{this.constructor.name}</h1>
-      <p>
-        Edit the content of this page in /src/pages/ExamplePages.js
-      </p>
-    </div>
-  );
-}
-
 class ExamplePage0 extends Component {
   render() {
-    return ret.apply(this);
+    return (
+      <div className="page">
+        <h1>ExamplePage0</h1>
+        <p>
+          Edit the content of this page in /src/pages/ExamplePages.js
+        </p>
+      </div>
+    );
   }
 }
 class ExamplePage1 extends Component {
   render() {
-    return ret.apply(this);
+    return (
+      <div className="page">
+        <h1>ExamplePage1</h1>
+        <p>
+          Edit the content of this page in /src/pages/ExamplePages.js
+        </p>
+      </div>
+    );
   }
 }
 class ExamplePage2 extends Component {
   render() {
-    return ret.apply(this);
+    return (
+      <div className="page">
+        <h1>ExamplePage2</h1>
+        <p>
+          Edit the content of this page in /src/pages/ExamplePages.js
+        </p>
+      </div>
+    );
   }
 }
 class ExamplePage3 extends Component {
   render() {
-    return ret.apply(this);
+    return (
+      <div className="page">
+        <h1>ExamplePage3</h1>
+        <p>
+          Edit the content of this page in /src/pages/ExamplePages.js
+        </p>
+      </div>
+    );
   }
 }
 class ExamplePage4 extends Component {
   render() {
-    return ret.apply(this);
+    return (
+      <div className="page">
+        <h1>ExamplePage4</h1>
+        <p>
+          Edit the content of this page in /src/pages/ExamplePages.js
+        </p>
+      </div>
+    );
   }
 }
 
